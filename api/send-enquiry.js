b3442fd2932acbeec83d16b1d9207a31fcf2b06b
Tymofii <tymofii@tattoodima.com>
2026-05-11 16:30:35 +0200

    fix: bypass Netlify limits by directly calling Vercel API and adding CORS headers

--- api/send-enquiry.js
+++ api/send-enquiry.js
@@ -1,6 +1,17 @@
 const Busboy = require('busboy');
 
 module.exports = async (req, res) => {
+    // Enable CORS
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     console.log('--- ENQUIRY REQUEST RECEIVED ---');
     if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
 
